2f10ee36aa872392586188c5e33b013a9fd85c15
erincocker1 <erin.cocker@ten10.com>
2025-02-04 12:15:20 +0000

    some more challenges and exercises

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,13 +2,9 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        PrimitiveTypes primitiveTypes = new PrimitiveTypes();
-        primitiveTypes.challenge();
+public class App {
+    public static void main(String[] args) {
+        SumAndAverage.inputThenPrintSumAndAverage();
     }
 }
---
+++ src/main/java/org/example/BucketsOfPaint.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class BucketsOfPaint {
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+
+        double remainingArea = width*height - areaPerBucket*extraBuckets;
+        return (int) Math.ceil(remainingArea/areaPerBucket);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        return getBucketCount(width, height, areaPerBucket, 0);
+    }
+}
