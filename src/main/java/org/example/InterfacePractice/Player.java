5d2e412a52349dc65799ee02e0cbc124ecb98c75
erincocker1 <erin.cocker@ten10.com>
2025-02-12 14:25:47 +0000

    another exercise

---
+++ src/main/java/org/example/InterfacePractice/Player.java
@@ -0,0 +1,74 @@
+package org.example.InterfacePractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements ISaveable {
+    private String name;
+    private String weapon;
+    private int hitPoints;
+    private int strength;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.weapon = "Sword";
+    }
+
+    @Override
+    public List<String> write() {
+        return new ArrayList<String>(List.of(name, Integer.toString(hitPoints),
+                Integer.toString(strength), weapon));
+    }
+
+    @Override
+    public void read(List<String> list) {
+        if (list == null || list.size() == 0) {
+            return;
+        }
+        this.name = list.get(0);
+        this.hitPoints = Integer.parseInt(list.get(1));
+        this.strength = Integer.parseInt(list.get(2));
+        this.weapon = list.get(3);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{name='%s', hitPoints=%d, strength=%d, weapon='%s'}"
+                .formatted(name, hitPoints, strength, weapon);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+}
