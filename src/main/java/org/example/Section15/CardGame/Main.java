a83979aab6713e8433c720f93ce26b4e7aafb77e
erincocker1 <erin.cocker@ten10.com>
2025-02-18 12:05:57 +0000

    printBoard method is implemented, showing the state of the board

--- src/main/java/org/example/Section15/CardGame/Main.java
+++ src/main/java/org/example/Section15/CardGame/Main.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Solitaire solitaire = new Solitaire();
-
+        solitaire.printBoard();
     }
 }
--- src/main/java/org/example/Section15/CardGame/Solitaire.java
+++ src/main/java/org/example/Section15/CardGame/Solitaire.java
@@ -3,20 +3,30 @@ package org.example.Section15.CardGame;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 public class Solitaire {
     private List<List<Card>> tableau;
-    private List<Card> stock;
+    private Stack<Card> stock;
+    private Stack<Card> wastePile;
+    private Stack<Card> hearts;
+    private Stack<Card> diamonds;
+    private Stack<Card> spades;
+    private Stack<Card> clubs;
 
     public Solitaire() {
+        tableau = new ArrayList<>(7);
+        stock = new Stack<>();
+        wastePile = new Stack<>();
+        hearts = new Stack<>();
+        diamonds = new Stack<>();
+        spades = new Stack<>();
+        clubs = new Stack<>();
+
         SetUpBoard();
-        System.out.println(tableau);
-        System.out.println("---------");
-        System.out.println(stock);
     }
 
     private void SetUpBoard() {
-        tableau = new ArrayList<>(7);
         for (int i = 0; i < 7; i++) {
             tableau.add(new ArrayList<>());
         }
@@ -24,12 +34,36 @@ public class Solitaire {
         List<Card> deck = Card.getStandardDeck(false);
         Collections.shuffle(deck);
         for (int height = 0; height < 7; height++) {
-            for (int pile = 0; pile < 6-height; pile++) {
+            for (int pile = height+1; pile < 7; pile++) {
                 tableau.get(pile).add(deck.remove(0));
             }
-            tableau.get(6-height).add(deck.remove(0).flip());
+            tableau.get(height).add(deck.remove(0).flip());
         }
 
-        stock = new ArrayList<>(deck);
+        stock.addAll(deck);
     }
+
+    public void printBoard() {
+        System.out.printf("%5s%5s        %5s%5s%5s%5s\n",
+                getTopCard(stock), getTopCard(wastePile), getTopCard(hearts),
+                getTopCard(spades), getTopCard(clubs), getTopCard(diamonds));
+        System.out.println();
+
+        int maxHeight = 0;
+        for (List<Card> pile : tableau) {
+            maxHeight = Math.max(pile.size(), maxHeight);
+        }
+
+        for (int height = 0; height < maxHeight; height++) {
+            for (int pile = 0; pile < 7; pile++) {
+                System.out.printf("%5s", height < tableau.get(pile).size() ? tableau.get(pile).get(height) : "");
+            }
+            System.out.println();
+        }
+    }
+
+    private String getTopCard(Stack<Card> stack) {
+        return stack.isEmpty() ? "[ ]" : stack.peek().toString();
+    }
+
 }
