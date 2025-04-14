7c61d3f796de70248a2daae97ed9e52d07b361bf
erincocker1 <erin.cocker@ten10.com>
2025-04-14 17:06:07 +0100

    http post request challenge complete

--- src/main/java/org/example/section23/ConcurrentRequests.java
+++ src/main/java/org/example/section23/ConcurrentRequests.java
@@ -1,15 +1,19 @@
 package org.example.section23;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
 import java.util.concurrent.CompletableFuture;
 
 public class ConcurrentRequests {
+    private static final Path orderTracking = Path.of("src/main/java/org/example/section23/order_data.txt");
 
     public static void main(String[] args) {
 
@@ -24,15 +28,51 @@ public class ConcurrentRequests {
 
         String urlBase = "http://localhost:8080";
 
-        List<URI> sites = new ArrayList<>();
-        orderMap.forEach( (k,v) -> sites.add(URI.create(
-                urlBase + "?" + urlparams.formatted(k, v)
-        )));
+//        List<URI> sites = new ArrayList<>();
+//        orderMap.forEach( (k,v) -> sites.add(URI.create(
+//                urlBase + "?" + urlparams.formatted(k, v)
+//        )));
 
         HttpClient client = HttpClient.newHttpClient();
-        sendGets(client, sites);
+//        sendGets(client, sites);
+
+
+        if (!Files.exists(orderTracking)) {
+            try {
+                Files.createFile(orderTracking);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        sendPosts(client, URI.create(urlBase), urlparams, orderMap);
     }
 
+    private static void sendPosts(HttpClient client, URI baseUri, String requestParams, Map<String, Integer> orders) {
+        var futures = orders.entrySet().stream()
+                .map(entry -> requestParams.formatted(entry.getKey(),entry.getValue()))
+                .map(body -> HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers.ofString(body)))
+                .map(builder -> builder.uri(baseUri))
+                .map(HttpRequest.Builder::build)
+                .map(request -> client.sendAsync(request, HttpResponse.BodyHandlers.ofString()))
+                .toList();
+
+        var allFutureRequests = CompletableFuture.allOf(
+                futures.toArray(new CompletableFuture<?>[0])
+        );
+
+        allFutureRequests.join();
+
+        StringJoiner responseData = new StringJoiner("\n");
+        futures.forEach(f -> {
+            responseData.add(f.join().body());
+        });
+
+        try {
+            Files.writeString(orderTracking, responseData.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     private static void sendGets(HttpClient client, List<URI> uris) {
 
         var futures = uris.stream()
@@ -52,4 +92,5 @@ public class ConcurrentRequests {
             System.out.println(f.join().body());
         });
     }
+
 }
