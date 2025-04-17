cbbd5c54eadc7bc184c33132ec4bb51f5a2bc01c
erincocker1 <erin.cocker@ten10.com>
2025-04-17 12:15:42 +0100

    lesson 416 challenges completed

--- src/test/java/org/example/section24/UtilitiesTest.java
+++ src/test/java/org/example/section24/UtilitiesTest.java
@@ -1,35 +1,50 @@
 package org.example.section24;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilitiesTest {
+    Utilities util;
+    @BeforeEach
+    void setUp() {
+        util = new Utilities();
+    }
 
     @Test
     void everyNthChar() {
-        fail("Test not yet implemented");
+        assertArrayEquals(new char[] {'e','l'}, util.everyNthChar("hello".toCharArray(), 2));
+    }
+
+    @Test
+    void everyNthCharArrayNotLongEnough() {
+        assertArrayEquals(new char[] {'h','e','l','l','o'},
+                util.everyNthChar("hello".toCharArray(), 6));
     }
 
     @Test
     void removePairs() {
-        Utilities utilities = new Utilities();
-        assertEquals("ABCDEF", utilities.removePairs("AABCDDEFF"));
+        assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
     }
 
     @Test
     void removePairsWithNonConsecutiveRepeatingLetters() {
-        Utilities utilities = new Utilities();
-        assertEquals("ABCABDEF", utilities.removePairs("ABCCABDEEF"));
+        assertEquals("ABCABDEF", util.removePairs("ABCCABDEEF"));
     }
 
     @Test
     void converter() {
-        fail("Test not yet implemented");
+        assertEquals(300, util.converter(10,5));
     }
 
     @Test
-    void nullIfOddLength() {
-        fail("Test not yet implemented");
+    void nullIfOddLengthOddString() {
+        assertNull(util.nullIfOddLength("odd"));
+    }
+
+    @Test
+    void nullIfOddLengthEvenString() {
+        assertEquals("even", util.nullIfOddLength("even"));
     }
 }
